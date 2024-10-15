eff07c2d6db072f8afd0ed72addcfbf13dbf4e7b
mazode <mrmaazkhan191@gmail.com>
2024-10-15 22:12:56 +0500

    added js logic

--- 2-Fizz-Buzz/script.js
+++ 2-Fizz-Buzz/script.js
@@ -1,13 +1,23 @@
-function fizzBuzzProgram(input) {
-    let result = '';
+documemt.getElementById("startButton").addEventListener("click", function() {
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
 
-    for(let i = 0; i < input.length; i++) {
-        if(input % 3 == 0 && input % 5 == 0) {
-            return 'FizzBuzz';
-        } else if(input % 3 == 0) {
-            return 'Fizz';
-        } else if(input % 5 == 0) {
-            return 'Buzz';
+    for(let i = 1; i <= 100; i++) {
+        const result = document.createElement("div");
+
+        if(i % 3 === 0 && i % 5 === 0) {
+            result.textContent = 'FizzBuzz';
+            result.classList.add("fizzbuzz");
+        } else if(i % 3 === 0) {
+            result.textContent = 'Fizz';
+            result.classList.add("fizz");
+        } else if(i % 5 === 0) {
+            result.textContent = 'Buzz';
+            result.classList.add("buzz");
+        } else {
+            result.textContent = i;
         }
+
+        resultDiv.appendChild(result);
     }
-}
+}); 
